c4981e3b118439ddd716745b0663a713e58e2959
Moritz Moldenhauer <moritz.moldenhauer@gmail.com>
2020-11-25 15:22:55 +0100

    This code is now working. Needed to remove the & sign, was not working with npm start.
    Added launch.json too.

--- section_6_Working_with_Dynamic_Content_Adding_Templating_Engines/assignment_moritz/app.js
+++ section_6_Working_with_Dynamic_Content_Adding_Templating_Engines/assignment_moritz/app.js
@@ -3,9 +3,9 @@ const express = require('express');
 
 const app = express();
 
+const users = [];
 // set templating/view engine to EJS
 app.set('view engine', 'ejs');
-app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -15,8 +15,22 @@ app.get('/', (req, res, next) => {
   });
 });
 
+app.get('/users', (req, res, next) => {
+  res.render('users', {
+    pageTitle: 'User',
+    users: users,
+  });
+});
+
+app.post('/add-user', (req, res, next) => {
+  users.push({ name: req.body.username });
+  res.redirect('/users');
+});
+
 app.use((req, res, next) => {
   res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
